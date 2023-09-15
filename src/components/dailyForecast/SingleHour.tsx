b5cd5f91de0d81ec39d00cf5571362fd9e00f958
JUANI\juani <juanisabbatini@gmail.com>
2023-09-15 21:27:31 +0200

    display remaining hours of the day and the hours from tomorrow

---
+++ src/components/dailyForecast/SingleHour.tsx
@@ -0,0 +1,24 @@
+import moment from "moment";
+import { Hour } from "../../interfaces/weather.interface";
+import styles from "./dailyForecast.module.less"
+
+interface Props{
+    hour: Hour;
+    index: number;
+    selected: number;
+    onClick: (hour: Hour, index: number) => void;
+}
+
+export const SingleHour = ({hour, index, selected, onClick}: Props) => {
+  return (
+    <div
+    onClick={() => onClick(hour, index)}
+    className={`${styles.hoursList} ${
+      selected === index ? styles.selected : styles.unselected
+    }`}
+  >
+    <p>{index === 0 ? "Now" : moment(hour.time).format("HH")}</p>
+    <img src={hour.condition.icon} alt={hour.condition.text} />
+    <p>{Math.round(hour.temp_c)}°C</p>
+  </div>  )
+}
